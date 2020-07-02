50dd78af911d3bafa18c20bd631e1258b35d77c7
yangbing <youngbing0920@163.com>
2020-07-02 10:16:48 +0800

    【+】提交demo

---
+++ springboot-kafka/src/main/java/com/young/springbootkafka/SpringbootKafkaApplication.java
@@ -0,0 +1,16 @@
+package com.young.springbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author young
+ */
+@SpringBootApplication
+public class SpringbootKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKafkaApplication.class, args);
+    }
+
+}
