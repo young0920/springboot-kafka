90608f7c9a0c252085620f4e5f8bc713c9336d44
yangbing <youngbing0920@163.com>
2020-07-10 17:07:18 +0800

    []

--- springboot-kafka/src/main/java/com/young/springbootkafka/config/ExecutorConfig.java
+++ springboot-kafka/src/main/java/com/young/springbootkafka/config/ExecutorConfig.java
@@ -5,7 +5,6 @@ import com.young.springbootkafka.thread.threadpool.VisiableThreadPoolTaskExecuto
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
@@ -20,7 +19,6 @@ import java.util.concurrent.ThreadPoolExecutor;
  * @date 2020/7/10 11:06
  */
 @Configuration
-@EnableAsync
 @Slf4j
 public class ExecutorConfig {
 
