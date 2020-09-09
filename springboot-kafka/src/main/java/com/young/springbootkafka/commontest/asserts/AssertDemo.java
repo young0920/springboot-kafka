9a4deca000eea58a56d81d33e614fd5538a4ef33
yangbing <youngbing0920@163.com>
2020-09-09 15:46:55 +0800

    [*] Assert 断言

--- springboot-kafka/src/main/java/com/young/springbootkafka/commontest/asserts/AssertDemo.java
+++ springboot-kafka/src/main/java/com/young/springbootkafka/commontest/asserts/AssertDemo.java
@@ -1,5 +1,9 @@
 package com.young.springbootkafka.commontest.asserts;
 
+import com.young.springbootkafka.pojo.User;
+import com.young.springbootkafka.pojo.User2;
+import org.springframework.util.Assert;
+
 /**
  * AssertDemo
  *
@@ -7,4 +11,15 @@ package com.young.springbootkafka.commontest.asserts;
  * @Date 2020/9/8 10:21 上午
  */
 public class AssertDemo {
+    public static void main(String[] args) {
+        Assert.hasText(" xx","不能为空");
+
+        Assert.notNull("null","不能为null");
+
+        Assert.isNull(null,"shi");
+
+        Assert.isInstanceOf(User.class,new User2(),"是");
+
+
+    }
 }
