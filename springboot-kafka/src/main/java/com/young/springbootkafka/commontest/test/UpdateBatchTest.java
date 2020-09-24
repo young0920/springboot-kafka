237ebde3dbff50f688f88752554484a1b28348c4
yangbing <youngbing0920@163.com>
2020-09-24 17:58:55 +0800

    [*]测试代码

--- springboot-kafka/src/main/java/com/young/springbootkafka/commontest/test/UpdateBatchTest.java
+++ springboot-kafka/src/main/java/com/young/springbootkafka/commontest/test/UpdateBatchTest.java
@@ -16,19 +16,19 @@ public class UpdateBatchTest {
     }
 
     private static void getUpdateBatch(String s, String idC) {
-        StringBuilder sb = new StringBuilder("<trim prefix=\"set\" suffixOverrides=\",\">\n");
+        StringBuilder sb = new StringBuilder("        <trim prefix=\"set\" suffixOverrides=\",\">\n");
         String[] split = s.split("\n");
         for (String s1 : split) {
             String s2 = s1.replace(",", " ").trim();
             String[] split1 = s2.split("=");
             String s3 = split1[1].trim().split(" ")[0].replace("#{", "");
-            sb.append("<trim prefix=\"").append(split1[0]).append("=case\" suffix=\"end,\">\n");
-            sb.append("<foreach collection=\"list\" item=\"item\" index=\"index\">\n");
-            sb.append("<if test=\"item.").append(s3).append("!=null\">\n");
-            sb.append("when ").append(idC).append(" then #{item.").append(s3).append("}\n");
-            sb.append("</if>\n").append("</foreach>\n").append("</trim>\n");
+            sb.append("            <trim prefix=\"").append(split1[0]).append("=case\" suffix=\"end,\">\n");
+            sb.append("               <foreach collection=\"list\" item=\"item\" index=\"index\">\n");
+            sb.append("                    <if test=\"item.").append(s3).append("!=null\">\n");
+            sb.append("                        when ").append(idC).append(" then #{item.").append(s3).append("}\n");
+            sb.append("                    </if>\n").append("                </foreach>\n").append("            </trim>\n");
         }
-        sb.append("</trim>");
+        sb.append("        </trim>");
         System.out.println(sb.toString());
     }
 }
