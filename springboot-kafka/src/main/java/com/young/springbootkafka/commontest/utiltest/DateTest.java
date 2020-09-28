a7b8a93d6503038d1badd6536d9943b1599c4c45
yangbing <youngbing0920@163.com>
2020-09-28 20:44:22 +0800

    [*] 过滤器与拦截器

--- springboot-kafka/src/main/java/com/young/springbootkafka/commontest/utiltest/DateTest.java
+++ springboot-kafka/src/main/java/com/young/springbootkafka/commontest/utiltest/DateTest.java
@@ -1,7 +1,10 @@
 package com.young.springbootkafka.commontest.utiltest;
 
+import org.apache.commons.lang3.time.DateFormatUtils;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -20,6 +23,9 @@ public class DateTest {
     private static void printDate(String dateStart, String dateEnd) throws ParseException {
         //        DateFormatUtils.format()
         SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
+        String format = DateFormatUtils.format(new Date(), "yyyy-MM-dd");
+        String format1 = DateFormatUtils.format(System.currentTimeMillis(), "yyyy-MM-dd");
+        String format2 = DateFormatUtils.format(Calendar.getInstance(), "yyyy-MM-dd");
         long startTime = date.parse(dateStart).getTime();
         long endTime = date.parse(dateEnd).getTime();
         long day = 1000 * 60 * 60 * 24;
