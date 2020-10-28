c105570c4d92be841ce35532773689824cc826d3
yangbing <youngbing0920@163.com>
2020-10-28 14:18:05 +0800

    [*]leetcode

---
+++ springboot-kafka/src/main/java/com/young/springbootkafka/commontest/leetcode/editor/cn/P21MergeTwoSortedLists .java
@@ -0,0 +1,64 @@
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+//
+// 
+//
+// 示例： 
+//
+// 输入：1->2->4, 1->3->4
+//输出：1->1->2->3->4->4
+// 
+// Related Topics 链表 
+// 👍 1345 👎 0
+
+
+package com.young.springbootkafka.commontest.leetcode.editor.cn;
+
+import com.young.springbootkafka.pojo.ListNode;
+
+/**
+ * 合并两个有序链表
+ * @author young
+ */ 
+class P21MergeTwoSortedLists{
+    public static void main(String[] args) {
+        Solution solution = new P21MergeTwoSortedLists().new Solution();
+        // TO TEST
+    }
+    /**
+    * 合并两个有序链表
+    */ 
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null){
+            return l2;
+        }
+
+        if (l2 == null){
+            return  l1;
+        }
+        //如果11的val值更小，则将11.next与排序好的链表头相接，12同理
+        if(l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            //每一层调用都返回排序好的链表头
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
