8ef48189f24eb396967403f468fc4654c93d953f
yangbing <youngbing0920@163.com>
2020-12-25 13:19:21 +0800

    [*]

--- springboot-kafka/src/main/resources/static/js/index.js
+++ springboot-kafka/src/main/resources/static/js/index.js
@@ -1,41 +1,84 @@
 var app = new Vue({
-  el: "#app",
-  data: {
-    message: "Hello Springboot!",
-    Janu: '',
-    date: '',
-  },
-  mounted() { },
-  methods: {
-    getDate() {
-      let date = new Date();
-      let month = date.getMonth() + 1;
-      month = month.toString().padStart(2, "0");
-      return date.getFullYear() + "-" + month;
+    el: "#app",
+    data: {
+        message: "Hello Springboot!",
+        Janu: '',
+        date: '',
+        fileList: [],
+        a_fileList: [],
     },
-    async post() {
-      var param = {};
-      fetch(url, {
-        headers: { contentType: "application/json;utf8" },
-        method: "post",
-        body: JSON.stringify(param),
-      })
-        .then((s) => {
-          let result = s.date;
-          if (result.code === 1) {
-            console(result.message);
-            return result.message;
-          }
-        })
-        .catch(function (error) {
-          alert(error);
-        });
+    mounted() {
+    },
+    methods: {
+        _changeFile(event) {
+            let a = event.target.files;
+            console.log(a);
+            console.log(a[0]);
+            this.a_fileList.push(a[0]);
+            console.log(this.a_fileList)
+        },
+        async _upload() {
+            let fd = new FormData()
+            this.a_fileList.map((item, index) => {
+                fd.append('file' + index, item);
+            });
+            let res = await fetch("file/upload", {
+                method: 'post',
+                body: fd
+            }).then(res => res.json());
 
-      let mesaage = await fetch(url, {
-        headers: { contentType: "application/json;utf8" },
-        method: "post",
-        body: JSON.stringify(param),
-      }).then((s) => s.json());
+            console.log(res);
+
+        },
+        submitUpload() {
+            this.$refs.upload.submit();
+        },
+        //移除方法
+        handleRemove(file, fileList) {
+            console.log(file, fileList);
+        },
+        changeFile(file, fileList) {
+            console.log(file);
+            console.log(fileList);
+        },
+        uploadSuccess(response, file, fileList) {
+            console.log(response);
+            console.log(file);
+            console.log(fileList);
+        },
+        //预览方法
+        handlePreview(file) {
+            console.log(file);
+        },
+        getDate() {
+            let date = new Date();
+            let month = date.getMonth() + 1;
+            month = month.toString().padStart(2, "0");
+            return date.getFullYear() + "-" + month;
+        },
+        async post() {
+            var param = {};
+            fetch(url, {
+                headers: {contentType: "application/json;utf8"},
+                method: "post",
+                body: JSON.stringify(param),
+            })
+                .then((s) => {
+                    let result = s.date;
+                    if (result.code === 1) {
+                        console(result.message);
+                        return result.message;
+                    }
+                })
+                .catch(function (error) {
+                    alert(error);
+                });
+
+            let mesaage = await fetch(url, {
+                headers: {contentType: "application/json;utf8"},
+                method: "post",
+                body: JSON.stringify(param),
+            }).then((s) => s.json());
+        },
     },
-  },
 });
